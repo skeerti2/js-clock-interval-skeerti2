038669474b47f5957ae034796d1424c5523e4b0a
Sruti Keerti <skeerti2@gmail.com>
2017-01-26 00:17:15 +0800

    namespace-attempt

--- clock.js
+++ clock.js
@@ -2,51 +2,72 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 //location.reload()
-timerId = setInterval(clockImplementation, 1000)
+//timerId = setInterval(clockImplementation, 1)
 //location.reload()//clockImplementation()
 
-function clockImplementation() {
-  var now = new Date()
-var hourNow = document.getElementById('hour').innerHTML = now.getHours();
+var clockImplementation = {
+  //now : new Date(),
+  //hourNow : new Date().getHours(),
+  //document.getElementById('hour').innerHTML
 //console.log(hourNow)
-var minuteNow = document.getElementById('minute').innerHTML = now.getMinutes();
-var secondNow = document.getElementById('second').innerHTML = now.getSeconds();
+  //minuteNow : new Date().getMinutes(),
+  //document.getElementById('minute').innerHTML
+  //secondNow : new Date().getSeconds(),
+  // document.getElementById('second').innerHTML
 
 
-var clockFace = document.getElementById('clock')
+clockFace : document.getElementById('clock'),
 //console.log(clockFace)
 
-var minHand = document.getElementById('minute')
-clockFace.appendChild(minHand)
-var hourHand = document.getElementById('hour')
-clockFace.appendChild(hourHand)
-var secondHand = document.getElementById('second')
-clockFace.appendChild(secondHand)
+minHand : document.getElementById('minute'),
+//clockFace.appendChild(minHand)
+hourHand : document.getElementById('hour'),
+//clockFace.appendChild(hourHand)
+secondHand : document.getElementById('second'),
+//clockFace.appendChild(secondHand)
 
 //console.log(secondHand)
 //Move the second hand
-console.log("current time is "+ now)
-hourHand.style.transform = "rotate(" + hourRotation(hourNow)+ "deg)"
-minHand.style.transform = "rotate(" + minuteRotation(minuteNow)+ "deg)"
-secondHand.style.transform = "rotate(" + secondRotation(secondNow)+ "deg)"
+//console.log("current time is "+ now)
+// hourHand.style.transform = "rotate(" + hourRotation(hourNow)+ "deg)"
+// minHand.style.transform = "rotate(" + minuteRotation(minuteNow)+ "deg)"
+// secondHand.style.transform = "rotate(" + secondRotation(secondNow)+ "deg)"
 
-function minuteRotation(minuteValue)
+minuteRotation: function(minuteValue)
 {
   return (minuteValue/60)*360
-}
-function hourRotation(hourValue){
+},
+hourRotation: function (hourValue, minuteValue){
   if (hourValue === 12)
   {
     return 0;
   }
   else
   {
-  return ((hourValue/12)*360 + ((30/60)*minuteNow))
+  return ((hourValue/12)*360 + ((30/60)*minuteValue))
   }
-}
-function secondRotation(secondValue){
+},
+secondRotation: function (secondValue){
   return (secondValue/60)*360
+},
+updateClock : function(){
+  var now = new Date()
+  var hourNow = now.getHours()
+  var minuteNow = now.getMinutes()
+  var secondNow = now.getSeconds()
+  this.hourHand.style.transform = "rotate(" + this.hourRotation(hourNow, minuteNow)+ "deg)"
+  this.minHand.style.transform = "rotate(" + this.minuteRotation(minuteNow)+ "deg)"
+  this.secondHand.style.transform = "rotate(" + this.secondRotation(secondNow)+ "deg)"
 }
 
 }
+
+document.getElementById('hour').innerHTML = clockImplementation.hourNow
+document.getElementById('minute').innerHTML = clockImplementation.minuteNow
+document.getElementById('second').innerHTML = clockImplementation.secondNow
+clockImplementation.clockFace.appendChild(clockImplementation.hourHand)
+clockImplementation.clockFace.appendChild(clockImplementation.minHand)
+clockImplementation.clockFace.appendChild(clockImplementation.secondHand)
+var timerId = setInterval(clockImplementation.updateClock.bind(clockImplementation), 1)
+
 })
